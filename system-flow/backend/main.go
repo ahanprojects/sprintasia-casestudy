a6f417ac83decd607b29b8afcea5cc20899be304
Farhan <ahandnr@gmail.com>
2023-12-10 20:34:25 +0700

    ui done

--- system-flow/backend/main.go
+++ system-flow/backend/main.go
@@ -12,6 +12,19 @@ func init() {
 }
 func main() {
 	router := gin.Default()
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	})
 	controllers.GenerateRoutes(router)
 	router.Run()
 }
---
+++ system-flow/frontend/src/utils/mock.ts
@@ -0,0 +1,108 @@
+export const MOCK_DATA: any = [
+  {
+    "id": 24,
+    "created_at": "2023-12-10T14:21:31.918266+07:00",
+    "updated_at": "2023-12-10T14:21:31.918266+07:00",
+    "deleted_at": null,
+    "name": "Task 2",
+    "due": "0001-01-01T07:00:00+07:00",
+    "completed": false,
+    "parent_id": null,
+    "subtasks": [
+      {
+        "id": 32,
+        "created_at": "2023-12-10T07:30:36.908869Z",
+        "updated_at": "2023-12-10T07:30:36.908869Z",
+        "deleted_at": null,
+        "name": "Subtask 11",
+        "due": "0001-01-01T00:00:00Z",
+        "completed": false,
+        "parent_id": 24
+      },
+      {
+        "id": 34,
+        "created_at": "2023-12-10T07:40:24.858695Z",
+        "updated_at": "2023-12-10T07:40:24.858695Z",
+        "deleted_at": null,
+        "name": "Subtask 112",
+        "due": "0001-01-01T00:00:00Z",
+        "completed": false,
+        "parent_id": 24
+      },
+      {
+        "id": 35,
+        "created_at": "2023-12-10T07:40:28.243541Z",
+        "updated_at": "2023-12-10T07:40:28.243541Z",
+        "deleted_at": null,
+        "name": "Subtask 112",
+        "due": "0001-01-01T00:00:00Z",
+        "completed": false,
+        "parent_id": 24
+      },
+      {
+        "id": 36,
+        "created_at": "2023-12-10T07:40:31.658333Z",
+        "updated_at": "2023-12-10T07:40:31.658333Z",
+        "deleted_at": null,
+        "name": "Subtask 11212",
+        "due": "0001-01-01T00:00:00Z",
+        "completed": false,
+        "parent_id": 24
+      },
+      {
+        "id": 37,
+        "created_at": "2023-12-10T07:40:35.077892Z",
+        "updated_at": "2023-12-10T07:40:35.077892Z",
+        "deleted_at": null,
+        "name": "Subtask 112312",
+        "due": "0001-01-01T00:00:00Z",
+        "completed": false,
+        "parent_id": 24
+      }
+    ]
+  },
+  {
+    "id": 33,
+    "created_at": "2023-12-10T14:31:07.768406+07:00",
+    "updated_at": "2023-12-10T14:31:07.768406+07:00",
+    "deleted_at": null,
+    "name": "Task 5",
+    "due": "0001-01-01T07:00:00+07:00",
+    "completed": false,
+    "parent_id": null,
+    "subtasks": []
+  },
+  {
+    "id": 26,
+    "created_at": "2023-12-10T14:21:45.640817+07:00",
+    "updated_at": "2023-12-10T14:21:45.640817+07:00",
+    "deleted_at": null,
+    "name": "Task 4",
+    "due": "0001-01-01T07:00:00+07:00",
+    "completed": false,
+    "parent_id": null,
+    "subtasks": []
+  },
+  {
+    "id": 27,
+    "created_at": "2023-12-10T14:25:17.524264+07:00",
+    "updated_at": "2023-12-10T14:25:17.524264+07:00",
+    "deleted_at": null,
+    "name": "Task 5",
+    "due": "0001-01-01T07:00:00+07:00",
+    "completed": false,
+    "parent_id": null,
+    "subtasks": []
+  },
+  {
+    "id": 25,
+    "created_at": "2023-12-10T14:21:38.916628+07:00",
+    "updated_at": "2023-12-10T14:21:38.916628+07:00",
+    "deleted_at": null,
+    "name": "Task 3",
+    "due": "0001-01-01T07:00:00+07:00",
+    "completed": false,
+    "parent_id": null,
+    "subtasks": []
+  }
+]
